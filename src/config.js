4c9c9899a813aa8b2394b021e36fa625a51c748c
Melissa Lafranchise <pulchrit@gmail.com>
2019-07-25 14:56:09 -0700

    change DB_URL in config.js for heroku deployment

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postresql://noteful-admin@localhost/noteful'
+    DB_URL: process.env.DATABASE_URL || 'postresql://noteful-admin@localhost/noteful'
 };
